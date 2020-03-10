0d5edcc0957d0c2a0f0389440f451bd522d4d1eb
Mike Prior <mprior19xx@gmail.com>
2020-03-10 15:29:55 -0400

    hey

--- config/database.php
+++ config/database.php
@@ -4,11 +4,11 @@ class Database
     // Note: specify your own database credentials
     private $host = "localhost";
 
-    private $db_name = "db_movies";
+    private $db_name = "db_movies2";
 
     private $username = "root";
 
-    private $password = "root";
+    private $password = "";
 
     private static $instance = null;
     public $conn;
